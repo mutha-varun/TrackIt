2ac940f901e2a11862cb9ce794be9de044b2da1b
Varun Mutha <muthavarun07@gmail.com>
2026-05-02 22:17:15 +0530

    created the bankhomepagefile

---
+++ trackit/lib/screens/bank_homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BankHomepage extends StatefulWidget {
+  const BankHomepage({super.key});
+
+  @override
+  State<BankHomepage> createState() => _BankHomepageState();
+}
+
+class _BankHomepageState extends State<BankHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
